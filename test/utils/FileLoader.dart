bd8547d92407853f99a8c3b0b663f1522ff29214
Bruno <bm.oliveira.dev@gmail.com>
2019-03-12 11:18:41 +0000

    Refactored file loader and fixed lint warnings

--- test/utils/FileLoader.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:io';
-
-class FileUtils {
-  static Future<String> loadFile({String fileName, String extension = ".m3u"}) {
-    final file = new File('test_resources/$fileName$extension');
-    return file.readAsString();
-  }
-}
---
+++ test/utils/file_loader.dart
@@ -0,0 +1,7 @@
+import 'dart:io';
+
+class FileUtils {
+  static Future<String> loadFile(
+          {String fileName, String extension = '.m3u'}) =>
+      File('test_resources/$fileName$extension').readAsString();
+}
