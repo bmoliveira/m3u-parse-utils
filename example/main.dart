bb2d444c0dc6a3dd0300fdf72806f1cdaf53ce8c
Bruno <bm.oliveira.dev@gmail.com>
2019-04-08 17:47:48 +0100

    Updated Example with the new helper

--- example/main.dart
+++ example/main.dart
@@ -6,4 +6,9 @@ Future<void> main(List<String> arguments) async {
   final fileContent = await File('resources/example.m3u').readAsString();
   final List<M3uGenericEntry> listOfTracks = await parseFile(fileContent);
   print(listOfTracks);
+
+  // Organized categories
+  final categories =
+      sortedCategories(entries: listOfTracks, attributeName: 'group-title');
+  print(categories);
 }
