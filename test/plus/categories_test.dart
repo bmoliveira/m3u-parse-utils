7856f9eecfe41862264018af50dd4fdc90d2d5e8
Bruno <bm.oliveira.dev@gmail.com>
2019-04-08 17:43:35 +0100

    Added all tests for category titles

--- test/plus/categories_test.dart
+++ test/plus/categories_test.dart
@@ -4,9 +4,29 @@ import 'package:test/test.dart';
 import '../utils/file_loader.dart';
 
 void main() {
-  test('M3U_Plus multi line file', () async {
-    final playlist =
-        await parseFile(await FileUtils.loadFile(fileName: 'plus/multi_line'));
+  test('It supports unknow categories', () async {
+    final playlist = await parseFile(
+        await FileUtils.loadFile(fileName: 'plus/multi_line_categories'));
+
+    final categories =
+        sortedCategories(entries: playlist, attributeName: 'group-title');
+
+    expect(categories.keys.length, 4);
+  });
+
+  test('all unknow categories', () async {
+    final playlist = await parseFile(await FileUtils.loadFile(
+        fileName: 'plus/multi_line_all_unknown_categories'));
+
+    final categories =
+        sortedCategories(entries: playlist, attributeName: 'group-title');
+
+    expect(categories.keys.length, 1);
+  });
+
+  test('It supports no unknow categories', () async {
+    final playlist = await parseFile(await FileUtils.loadFile(
+        fileName: 'plus/multi_line_no_unknown_categories'));
 
     final categories =
         sortedCategories(entries: playlist, attributeName: 'group-title');
